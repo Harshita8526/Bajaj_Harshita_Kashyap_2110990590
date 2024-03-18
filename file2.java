2a68215c9489fc4f2bebbdbc9b52c7b77d33c7f0
Harshita Kashyap <124551399+Harshita8526@users.noreply.github.com>
2024-03-18 15:03:36 +0530

    file2.java

--- file2.java
+++ file2.java
@@ -1,1 +1,25 @@
-http
+const axios = require("axios");
+
+const url = "https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/createAccount";
+
+const data = {
+  name: "Harshita Kashyap",
+  email: "harshita0590.be21@chitkara.edu.in",
+  rollNumber: 2110990590,
+  phone: 8278950177,
+};
+
+async function makeReq() {
+  try {
+    const response = await axios.post(url, data);
+    console.log("Success:", response.data);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+makeReq();
+
+
+
+ accountNumber: 'BFHL0018679'
